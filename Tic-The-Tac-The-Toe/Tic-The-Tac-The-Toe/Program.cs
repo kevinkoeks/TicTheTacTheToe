2a223b4106b502587df3ccf7b1cb49bc9ce49f91
Kevin <kevinkoeks05@gmail.com>
2022-07-16 20:28:14 +0200

    Push Local version before pull

--- Tic-The-Tac-The-Toe/Tic-The-Tac-The-Toe/Program.cs
+++ Tic-The-Tac-The-Toe/Tic-The-Tac-The-Toe/Program.cs
@@ -29,12 +29,15 @@ public static class TicTacToe
         {
             board = new Board();
             totalNumOfTurns = 0;
-            //board.ShowBoard();
+            board.ShowBoard();
         }
 
         void PlayGame()
         {
-            //
+            while (true)
+            {
+
+            }
         }
 
         static bool CheckForWinner()
@@ -45,7 +48,14 @@ public static class TicTacToe
 
             bool HorizontalLine()
             {
+                for (int i = 0; i < 3; i++)
+                {
+                    bool hasWon = true;
+                    bool isValueX = false;
 
+
+                }
+                
             }
             bool VerticalLine()
             {
@@ -68,7 +78,52 @@ public static class TicTacToe
 
 public class Board
 {
+    public Row[] rows;
 
+    public Board() //Constructors
+    {
+        rows = new Row[3];
+        for (int i =0; i < 3; i++) //Will create the new rows
+        {
+            rows[i] = new Row();
+        }
+    }
+
+    public void ShowBoard()
+    {
+        for(int i = 0; i < rows.Length; i++)
+        {
+            rows[i].DisplayContents();
+        }
+    }
+
+    public void ChooseRowThenSpace()
+    {
+        string input = "";
+        int rowNum = 0;
+
+        Console.WriteLine("Choose row 1,2, or 3 (top to bottom):");
+        input = Console.ReadLine();
+
+        if(int.TryParse(input, out rowNum)) {
+            if (rowNum  < 1 || rowNum > 3)
+            {
+                Console.WriteLine("Input is Invalid. Please try again.:");
+                ChooseRowThenSpace(); //Recursion
+                return;
+            }
+            else
+            {
+                rows[rowNum - 1].ChooseSpace(TicTacToe.isPlayerATurn); //rowNum-1 -> because array index start at 0
+            }
+        }
+        else
+        {
+            Console.WriteLine("That's not a valid input, Please try again.:");
+            ChooseRowThenSpace();
+            return;
+        }
+    }
 }
 
 public class Row
